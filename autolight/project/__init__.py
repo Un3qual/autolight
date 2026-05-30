0a01b9fe317c9335506a5ef9c4b6b325c6732982
Ryan <Un3qual@users.noreply.github.com>
2026-05-30 12:32:35 -0700

    Add project domain models

---
+++ autolight/project/__init__.py
@@ -0,0 +1,21 @@
+from autolight.project.models import (
+    AudioAsset,
+    CacheEntry,
+    JobRun,
+    Marker,
+    ProjectDocument,
+    ResultState,
+    Track,
+    TrackType,
+)
+
+__all__ = [
+    "AudioAsset",
+    "CacheEntry",
+    "JobRun",
+    "Marker",
+    "ProjectDocument",
+    "ResultState",
+    "Track",
+    "TrackType",
+]
